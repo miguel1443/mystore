636008d642011a82fc3d70a493f65071534ca0f6
jaribgutierrez1 <jgutierr64@gmail.com>
2022-09-10 13:58:53 -0500

    inicializando el servidor de express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+//crear end point en el metodo GET
+
+app.get('/', function(req, res){
+    res.send('hello word');
+})
+
+app.listen(3000, () => {
+    console.log(`levantando server en el puerto ${port}`)
+});
